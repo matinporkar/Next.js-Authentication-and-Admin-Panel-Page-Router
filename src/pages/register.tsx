3072896660ae5038174a76b8bb70935ef84f4867
MatinPorkar <mrporkar.dev@gmail.com>
2026-02-20 19:16:04 +0330

    register form created and defualt styles deleted

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,6 +1,66 @@
+import { Formik, Form, Field, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+
+interface RegisterFormValues {
+    name: string,
+    email: string,
+    password: string
+}
+
 export default function Register() {
 
+    const initialValues: RegisterFormValues = {
+        name: "",
+        email: "",
+        password: ""
+    }
+
+    const validationSchema = Yup.object({
+        name : Yup.string().required("پر کردن این فیلد الزامیه"),
+        email : Yup.string().email("یک ایمیل معتبر وارد کن").required("پر کردن این فیلد الزامیه"),
+        password : Yup.string().min(6 , "حداقل 6 کاراکتر الزامیه").required("پر کردن این فیلد الزامیه")
+    })
+
+    const registerFormHandler = (values: RegisterFormValues , actions) => {
+        console.log(values)
+        actions.resetForm()
+    }
+
     return (
-        <h1>register page</h1>
+        <>
+            <div>
+                <h1>Register Form</h1>
+
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={registerFormHandler}
+                >
+                    <Form>
+
+                        <div>
+                            <label>name : </label>
+                            <Field name="name" type="text" />
+                            <ErrorMessage name="name" />
+                        </div>
+
+                        <div>
+                            <label>email : </label>
+                            <Field name="email" type="email" />
+                            <ErrorMessage name="email" />
+                        </div>
+
+                        <div>
+                            <label>password : </label>
+                            <Field name="password" type="password" />
+                            <ErrorMessage name="password" />
+                        </div>
+
+                        <button type='submit'>Submit</button>
+
+                    </Form>
+                </Formik>
+            </div>
+        </>
     )
 }
