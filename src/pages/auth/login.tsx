2cbc8c3853053c72a6ab5dfacbd100c6baf35cf0
MatinPorkar <mrporkar.dev@gmail.com>
2026-02-21 12:51:42 +0330

    login page created

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,27 +4,24 @@ import * as Yup from 'yup'
 import Input from '../../../app/components/shared/input'
 import Link from 'next/link'
 
-interface RegisterFormValues {
-    name: string,
+interface LoginFormValues {
     email: string,
     password: string
 }
 
-const Register: NextPage = () => {
+const Login: NextPage = () => {
 
-    const initialValues: RegisterFormValues = {
-        name: "",
+    const initialValues: LoginFormValues = {
         email: "",
         password: ""
     }
 
     const validationSchema = Yup.object({
-        name: Yup.string().required(),
         email: Yup.string().email().required(),
-        password: Yup.string().min(6).required()
+        password: Yup.string().required()
     })
 
-    const registerFormHandler = (values: RegisterFormValues, actions) => {
+    const loginFormHandler = (values: LoginFormValues, actions) => {
         console.log(values)
         actions.resetForm()
     }
@@ -33,16 +30,14 @@ const Register: NextPage = () => {
         <>
             <div className="min-h-screen flex items-center justify-center bg-gray-50 px-4">
                 <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8">
-                    <h1 className="text-2xl font-bold text-gray-800 text-center mb-2">Create Account</h1>
+                    <h1 className="text-2xl font-bold text-gray-800 text-center mb-2">Sign In</h1>
 
                     <Formik
                         initialValues={initialValues}
                         validationSchema={validationSchema}
-                        onSubmit={registerFormHandler}
+                        onSubmit={loginFormHandler}
                     >
                         <Form className="space-y-5">
-
-                            <Input labelTitle='name' inputName='name' Type='text' errorName='name' />
 
                             <Input labelTitle='email' inputName='email' Type='email' errorName='email' />
 
@@ -54,7 +49,7 @@ const Register: NextPage = () => {
                     </Formik>
 
                     <p className="text-xs text-center text-gray-400 mt-6">
-                        Already have an account? <Link href="/auth/login"><span className="underline cursor-pointer">Sign in</span></Link>
+                        Dont have an account? <Link href="/auth/register"><span className="underline cursor-pointer">Create Account</span></Link>
                     </p>
 
                 </div>
@@ -63,4 +58,4 @@ const Register: NextPage = () => {
     )
 }
 
-export default Register;
+export default Login;
