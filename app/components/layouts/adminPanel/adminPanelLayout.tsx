78d44a988f41e2ac423bf3e35ec40a7ba04a4866
MatinPorkar <mrporkar.dev@gmail.com>
2026-03-03 10:35:21 +0330

    product page tempelate created in adminPanel

--- app/components/layouts/adminPanel/adminPanelLayout.tsx
+++ app/components/layouts/adminPanel/adminPanelLayout.tsx
@@ -21,10 +21,9 @@ function classNames(...classes: string[]) {
 
 interface Props {
     children: ReactNode
-    pageName : string
 }
 
-const AdminPanelLayout = ({ children , pageName }: Props) => {
+const AdminPanelLayout = ({ children }: Props) => {
 
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -123,7 +122,6 @@ const AdminPanelLayout = ({ children , pageName }: Props) => {
                     <main className="flex-1">
                         <div className="py-6">
                             <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
-                                <h1 className="text-2xl font-semibold text-gray-900">{pageName}</h1>
                             </div>
                             <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
                                 {/* Replace with your content */}
